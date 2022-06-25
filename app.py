9b0be8b87b66f80e70f718dcfdd4f1c4ae5bebfd
PurvaSalunke <101494536+PurvaSalunke@users.noreply.github.com>
2022-06-25 18:48:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import streamlit as st
 
 #to create the heading
 st. set_page_config(layout="wide")
-st.header("Election Party Pradiction")
+st.markdown("<h1 style='text-align: center; color: red;'>Election Party Prediction </h1>", unsafe_allow_html=True)
 
 def data_preprocessing(path):
     data = pd.read_excel(path, sheet_name="Election_Dataset_Two Classes")
